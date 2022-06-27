756292fdf1163fc70ab2556ceea22282ad3d44db
Leonardo Ojeda <perros_rebeldes@hotmail.com>
2022-06-27 16:42:02 -0500

    Finalizacion video 239 | Fin seccion

--- 08-compo-api/src/views/Pokemon.vue
+++ 08-compo-api/src/views/Pokemon.vue
@@ -1,12 +1,46 @@
 <template>
   <section>
-    <h1>Pokemon View</h1>
+    <h1 v-if="isLoading">Buscando...</h1>
+    <h1 v-else-if="errorMessage">{{errorMessage}}</h1>
+
+    <template v-else>
+      <h3>{{pokemon.name}}</h3>
+      <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+      <br>
+      <RouterLink :to="{name: 'pokemon-search'}">Regresar</RouterLink>
+    </template>
   </section>
 </template>
 
 <script>
+import { watch } from 'vue';
+import { useRoute, onBeforeRouteLeave } from 'vue-router'
+import usePokemon from "../composables/usePokemon";
 export default {
+  setup(){
+    const route = useRoute()
 
+    const {pokemon, isLoading, errorMessage, searchPokemon} = usePokemon(route.params.id)
+
+    watch(
+      () => route.params.id,
+      () => searchPokemon(route.params.id)
+    )
+
+    onBeforeRouteLeave(() => {
+      const answer = window.confirm('Esta seguro que desea salir?')
+      if(!answer){
+        return false
+      }
+    })
+
+    return {
+      errorMessage,
+      isLoading,
+      pokemon,
+      searchPokemon
+    }
+  }
 }
 </script>
 
--- 08-compo-api/src/views/SearchPokemon.vue
+++ 08-compo-api/src/views/SearchPokemon.vue
@@ -2,7 +2,7 @@
   <section>
     <h1>Buscar pokemon: {{pokemonId}}</h1>
     <form @submit.prevent="onSubmit" action="">
-      <input type="number" placeholder="Numero del pokemon" autofocus v-model="pokemonId">
+      <input type="number" placeholder="Numero del pokemon" v-model="pokemonId" ref="txtSearchId">
     </form>
     <br>
     <span>Presione Enter para buscar</span>
@@ -17,8 +17,10 @@ export default {
     const router = useRouter()
 
     const pokemonId = ref(1)
+    const txtSearchId = ref()
     return {
       pokemonId,
+      txtSearchId,
       onSubmit: () => {
         console.log('Pokemon a buscar ', pokemonId.value);
         router.push({name: 'pokemon-id', params: {
